3356d428b1c6d399a62def4350a955f2e8af7966
mr-sandip <animeleaker121@gmail.com>
2026-07-17 17:54:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ app.listen(PORT, () => {
 });
 
 const bot = mineflayer.createBot({
-  host: "BinZaid.aternos.me",
-  port: 33268,
+  host: "mr_sandip.aternos.me",
+  port: 62409,
   username: "BinZaid",
   version: "1.20.1"
 });
